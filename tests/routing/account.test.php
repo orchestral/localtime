a6c6879c8aeefa9cf07e8d173239a3ae97918e87
crynobone <crynobone@gmail.com>
2013-01-21 23:13:38 +0800

    Add routing test case
    Signed-off-by: crynobone <crynobone@gmail.com>

---
+++ tests/routing/account.test.php
@@ -0,0 +1,69 @@
+<?php
+
+Bundle::start('orchestra');
+Bundle::start('localtime');
+
+class RoutingAccountTest extends Localtime\Testable\TestCase {
+	/**
+	 * User instance.
+	 *
+	 * @var Orchestra\Model\User
+	 */
+	protected $user = null;
+
+	/**
+	 * Setup the test environment.
+	 */
+	public function setUp()
+	{
+		Config::set('application.timezone', 'UTC');
+
+		parent::setUp();
+
+		$this->user = Orchestra\Model\User::find(1);
+		$this->be($this->user);
+	}
+
+	/**
+	 * Teardown the test environment.
+	 */
+	public function tearDown()
+	{
+		unset($this->user);
+
+		parent::tearDown();
+	}
+
+	/**
+	 * Test configuring user localtime.
+	 *
+	 * @test
+	 */
+	public function testConfigureUserLocaltime()
+	{
+		$meta = Orchestra\Memory::make('user');
+
+		$this->assertTrue(is_null($meta->get("timezone.1")));
+
+		$response = $this->call("orchestra::account@index", array(), 'POST', array(
+			'id'            => $this->user->id,
+			'fullname'      => 'Foobar',
+			'email'         => $this->user->email,
+			"meta_timezone" => "Asia/Kuala_Lumpur",
+		));
+
+		$this->assertInstanceOf('Laravel\Redirect', $response);
+		$this->assertEquals(handles('orchestra::account'), 
+			$response->foundation->headers->get('location'));
+
+		Orchestra\Core::shutdown();
+		Orchestra\Core::start();
+
+		$user = Orchestra\Model\User::find(1);
+		$date = "2011-08-11 08:00:00";
+
+		$this->assertEquals("Asia/Kuala_Lumpur", $meta->get("timezone.1"));
+		$this->assertEquals("Asia/Kuala_Lumpur", $user->timezone());
+		$this->assertEquals("2011-08-11 16:00:00", $user->localtime($date));
+	}
+}
